27602f4e2b26bb27b6683e54cf9d97d5ba136d0e
Patrycja Czajkowska <paczajkowska@gmail.com>
2018-10-15 21:03:07 +0200

    #1commit

---
+++ ObjectProgramming/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//obiekty
+		Pies dino = new Pies("Dino", "Dalmatyńczyk", 3, "Czarno-biały");
+		Pies burek = new Pies("Burek", "Owczarek", 5, "Brązowy");
+		
+		dino.szczekaj();
+		System.out.println(dino.getNazwa()+" "+dino.getRasa()+" "+dino.getWiek()+" "+dino.getKolor());	
+		System.out.println(dino.wiekZaKilkaLat(5));
+	
+		System.out.println("---------------------------");
+		
+		burek.szczekaj();
+		System.out.println(burek.getNazwa()+" "+burek.getRasa()+" "+burek.getWiek()+" "+burek.getKolor());
+		System.out.println(burek.wiekZaKilkaLat(5));
+		
+	}
+
+}
---
+++ ObjectProgramming/src/Pies.java
@@ -0,0 +1,67 @@
+
+public class Pies {
+	
+	//prywatne pola klasy określające jej stan
+
+	private String nazwa;
+	private String rasa;
+	private int wiek;
+	private String kolor;
+	
+	//konstruktor
+	public Pies(String nazwa, String rasa, int wiek, String kolor) {
+		super();
+		this.nazwa = nazwa; //this odróżnia pole obiektu od argumentu konstruktora
+		this.rasa = rasa;
+		this.wiek = wiek;
+		this.kolor = kolor;
+	}
+	
+	//metody określają zachowanie
+	void szczekaj() {
+		System.out.println("hau! hau!");
+	}
+	
+	int wiekZaKilkaLat(int lata) {
+		return wiek+lata;
+	}
+
+	//ustawienie dostepu do pol przez gettery i settery
+	public String getNazwa() {
+		return nazwa;
+	}
+
+	public void setNazwa(String nazwa) {
+		this.nazwa = nazwa;
+	}
+
+	public String getRasa() {
+		return rasa;
+	}
+
+	public void setRasa(String rasa) {
+		this.rasa = rasa;
+	}
+
+	public int getWiek() {
+		return wiek;
+	}
+
+	public void setWiek(int wiek) {
+		this.wiek = wiek;
+	}
+
+	public String getKolor() {
+		return kolor;
+	}
+
+	public void setKolor(String kolor) {
+		this.kolor = kolor;
+	}
+
+	
+	
+	
+	
+	
+}
